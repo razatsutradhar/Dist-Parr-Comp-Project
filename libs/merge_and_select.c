c4a52c4a6b9b392fe44f81b12b022d03c2d87d01
Ryan Jones <rtjones432@gmail.com>
2023-11-19 16:37:37 -0500

    added skeleton code for merge and select implementaiton

---
+++ libs/merge_and_select.c
@@ -0,0 +1,16 @@
+#include <mpi.h>
+
+// read arrays from data_files
+// send length to central
+// send arrays to central
+void edge() {}
+
+// query edge for number items
+// create central array
+// recieve sub arrays from edge
+// merge sub arrays
+// select rank and return
+void central() {}
+
+// run the merge and select
+void merge_and_select(char *data_files, int rank) {}
