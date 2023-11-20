fa7687d3fa6959550a729c6de401d755678af601
Ryan Jones <rtjones432@gmail.com>
2023-11-20 13:54:25 -0500

    added skeleton code for distributed selects

---
+++ libs/improved_distributed_select.c
@@ -0,0 +1,14 @@
+#include <mpi.h>
+
+// send range
+// on request send a random in range number
+// on request send the count lt and gt
+void worker() {}
+
+// recieve ranges and create a list
+// while true
+// select in range request in range
+// request all possible lt and gt filling in known based of range list
+void central() {}
+
+void improved_distributed_select(char *data_path, int rank) {}
