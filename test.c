8eb5a4238415be13810a4d4b7d0c4c66c1293eb6
Ryan Jones <rtjones432@gmail.com>
2023-12-06 17:39:04 -0500

    more tests

--- test.c
+++ test.c
@@ -10,23 +10,33 @@ int main(int argc, char *argv[]) {
   int result, i, j, world_rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-  int sims = 6;
-  FILE *b1, *m1, *b100, *m100, *b200, *m200;
-  if (world_rank == 0) {
-    b1 = fopen("./results/b1.txt", "w");
-    m1 = fopen("./results/m1.txt", "w");
-    b100 = fopen("./results/b100.txt", "w");
-    m100 = fopen("./results/m100.txt", "w");
-    b200 = fopen("./results/b200.txt", "w");
-    m200 = fopen("./results/m200.txt", "w");
-  }
-  FILE *files[6] = {b1, m1, b100, m100, b200, m200};
-  int rounds[6] = {100, 100, 100, 100, 100, 100};
+  int sims = 8;
   char *b, *m;
+  char *offset0, *offset100;
   asprintf(&b, "./data/base");
   asprintf(&m, "./data/same_mean");
-  char *data[6] = {b, m, b, m, b, m};
-  int rank[6] = {1, 1, 100, 100, 200, 200};
+  asprintf(&offset0, "./data/0_offset_100_std/test_2000_points");
+  asprintf(&offset100, "./data/200_offset_100_std/test_2000_points");
+  char *data[12] = {b,       m,         b,       m,         b,       m,
+                    offset0, offset100, offset0, offset100, offset0, offset100};
+  int rank[12] = {1, 1, 100, 100, 200, 200, 1, 1, 2000, 2000, 4000, 4000};
+  int rounds[12] = {100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100};
+  FILE *files[12];
+  if (world_rank == 0) {
+    files[0] = fopen("./results/b1.txt", "w");
+    files[1] = fopen("./results/m1.txt", "w");
+    files[2] = fopen("./results/b100.txt", "w");
+    files[3] = fopen("./results/m100.txt", "w");
+    files[4] = fopen("./results/b200.txt", "w");
+    files[5] = fopen("./results/m200.txt", "w");
+    files[6] = fopen("./results/0o20001.txt", "w");
+    files[7] = fopen("./results/100o20001.txt", "w");
+    files[8] = fopen("./results/0o20002000.txt", "w");
+    files[9] = fopen("./results/100o20002000.txt", "w");
+    files[10] = fopen("./results/0o20004000.txt", "w");
+    files[11] = fopen("./results/100o20004000.txt", "w");
+  }
+
   srand(world_rank + time(0));
   for (j = 0; j < sims; j++)
     for (i = 0; i < rounds[j]; i++) {
@@ -46,12 +56,8 @@ int main(int argc, char *argv[]) {
     }
 
   if (world_rank == 0) {
-    fclose(b1);
-    fclose(m1);
-    fclose(b100);
-    fclose(m100);
-    fclose(b200);
-    fclose(m200);
+    for (i = 0; i < sims; i++)
+      fclose(files[i]);
   }
   free(b);
   free(m);
